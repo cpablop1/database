8fb23cbb627cce67055627765afeeea104e9a71a
Rychy <jorgeajrhh@gmail.com>
2021-05-20 16:58:11 -0600

    Tablas de la BD terminadas

--- BD/app/SQL(Documentation)/tables.sql
+++ BD/app/SQL(Documentation)/tables.sql
@@ -4,8 +4,8 @@
 CREATE TABLE proveedor(
     nit INT UNSIGNED AUTO_INCREMENT,
     nombre_empresa VARCHAR(60),
-    representante_name VARCHAR(40),
-    representante_lastname VARCHAR(25),
+    prov_name VARCHAR(40),
+    prov_lastname VARCHAR(25),
     direccion VARCHAR(20),
     PRIMARY KEY(nit)
 ) ENGINE = INNODB;
@@ -35,9 +35,9 @@ CREATE TABLE permiso_sup(
     imprimir_cheque BOOLEAN,
     anular_cheque BOOLEAN,
     modificar_cheque BOOLEAN,
-    reporte_cheques BOOLEAN,
-    auditar_usuarios BOOLEAN,
-    admin_cuentas_bancaria BOOLEAN,
+    reporte_cheque BOOLEAN,
+    auditar_user BOOLEAN,
+    admin_cuenta_banc BOOLEAN,
     auditar_cuenta BOOLEAN,
     mostrar_bitacora_user BOOLEAN,
     mostrar_bitacora_group BOOLEAN,
@@ -48,11 +48,11 @@ CREATE TABLE permiso_sup(
 CREATE TABLE grupo(
     id_group INT UNSIGNED AUTO_INCREMENT,
     nombre VARCHAR(30),
-    montoMinimo DECIMAL(10, 2) UNSIGNED,
-    montoMaximo DECIMAL(10, 2) UNSIGNED,
-    nivelAutoridad INT UNSIGNED,
+    monto_min DECIMAL(10, 2) UNSIGNED,
+    monto_max DECIMAL(10, 2) UNSIGNED,
+    nivel_autoridad INT UNSIGNED,
     generar_cheque BOOLEAN,
-    llamar_jefe_Departamento BOOLEAN,
+    llamar_jefe_dep BOOLEAN,
     validar_cheque BOOLEAN,
     llamar_jefe_pagos BOOLEAN,
     PRIMARY KEY(id_group)
@@ -77,7 +77,6 @@ CREATE TABLE usuario(
     apellido VARCHAR(30),
     DPI BIGINT(15) UNSIGNED,
     direccion VARCHAR(20),
-    fecha_ingreso DATE,
     id_rol INT UNSIGNED,
     PRIMARY KEY(id_user),
     INDEX(id_rol),
@@ -212,11 +211,99 @@ CREATE TABLE bitacora_deposito(
 ) ENGINE = INNODB;
 
 -- Bitacoras huerfanas
--- por crear
+CREATE TABLE bitacora_personal(
+    id_b_personal INT UNSIGNED AUTO_INCREMENT,
+    id_user INT UNSIGNED,
+    nombre VARCHAR(45),
+    apellido VARCHAR(30),
+    DPI BIGINT(15) UNSIGNED,
+    direccion VARCHAR(20),
+    id_rol INT UNSIGNED,
+    fecha_ingreso DATE,
+    fecha_egreso DATE NULL,
+    PRIMARY KEY(id_b_personal)
+) ENGINE = INNODB; 
+
+CREATE TABLE bitacora_cuenta(
+    id_b_cuenta INT UNSIGNED AUTO_INCREMENT,
+    num_cuenta BIGINT(16) UNSIGNED,
+    nombre_banco VARCHAR(30),
+    nombre_cuenta VARCHAR(30),
+    fecha_creacion DATE,
+    fecha_eliminacion DATE NULL,
+    fondo DECIMAL(20, 2) UNSIGNED NULL,
+    PRIMARY KEY(id_b_cuenta)
+) ENGINE = INNODB;
+
+CREATE TABLE bitacora_cheque_fallido(
+    id_fallo INT UNSIGNED AUTO_INCREMENT,
+    fecha_fallo DATETIME,
+    cod_error VARCHAR(50),
+    id_user INT UNSIGNED,
+    id_cheque INT UNSIGNED,
+    PRIMARY KEY(id_fallo)
+) ENGINE = INNODB;
+
+CREATE TABLE bitacora_cheque_modificado(
+    id_mod INT UNSIGNED AUTO_INCREMENT,
+    fecha_mod DATETIME,
+    monto_antes DECIMAL(15, 2) UNSIGNED,
+    monto_post DECIMAL(15, 2) UNSIGNED,
+    benef_antes VARCHAR(67),
+    benef_post VARCHAR(67),
+    id_user INT UNSIGNED,
+    id_cheque INT UNSIGNED,
+    PRIMARY KEY(id_mod)
+) ENGINE = INNODB;
+
+CREATE TABLE bitacora_cheque_liberado(
+    id_liberacion INT UNSIGNED AUTO_INCREMENT,
+    fecha_liberacion DATETIME,
+    id_grupo INT UNSIGNED,
+    id_user INT UNSIGNED,
+    id_cheque INT UNSIGNED,
+    PRIMARY KEY(id_liberacion)
+) ENGINE = INNODB;
+
+CREATE TABLE bitacora_cheque_generado(
+    id_b_cheque INT UNSIGNED AUTO_INCREMENT,
+    fecha_gen DATETIME,
+    monto DECIMAL(15, 2) UNSIGNED,
+    lugar_gen VARCHAR(30),
+    beneficiario VARCHAR(67),
+    num_chequera INT UNSIGNED,
+    num_cuenta BIGINT(16) UNSIGNED,
+    id_user INT UNSIGNED,
+    id_cheque INT UNSIGNED,
+    PRIMARY KEY(id_b_cheque)
+) ENGINE = INNODB;
+
+-- Contactanos
+CREATE TABLE contactanos(
+    id_custom INT UNSIGNED AUTO_INCREMENT,
+    nombre VARCHAR(45),
+    num_telefono INT UNSIGNED,
+    correo VARCHAR(30),
+    mensaje VARCHAR(255),
+    estado BOOLEAN,
+    id_user INT UNSIGNED,
+    PRIMARY KEY(id_custom),
+    INDEX(id_user),
+    FOREIGN KEY(id_user) REFERENCES usuario(id_user) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 COMMIT;
 
 -- D R O P P I N G
+DROP TABLE contactanos;
+
+DROP TABLE bitacora_personal;
+DROP TABLE bitacora_cuenta;
+DROP TABLE bitacora_cheque_fallido;
+DROP TABLE bitacora_cheque_modificado;
+DROP TABLE bitacora_cheque_liberado;
+DROP TABLE bitacora_cheque_generado;
+
 DROP TABLE bitacora_deposito;
 DROP TABLE bitacora_cheque_emitido;
 DROP TABLE bitacora_movimiento_cuenta;
@@ -240,3 +327,4 @@ DROP TABLE permiso_sup;
 DROP TABLE correo_prov;
 DROP TABLE telefono_prov;
 DROP TABLE proveedor;
+COMMIT;
