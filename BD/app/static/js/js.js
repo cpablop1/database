971d15c5a4bab30b1844c3a76fdfb98ca3d88611
Willy Wolfsong <wolfsong.21@outlook.es>
2021-05-20 09:25:22 -0500

    Agregar vista de login

--- BD/app/static/js/js.js
+++ BD/app/static/js/js.js
@@ -1,5 +1,17 @@
-alert('Activado!!')
-console.log("Activado!!")
-// otro alert más!!
-console.log('Funciona pedazo de mierda!!')
-console.log('Funciona pedazo de mierda!!')
+'use strict'
+
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+    overlay = document.getElementById('overlay'),
+    popup = document.getElementById('popup'),
+    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function () {
+    overlay.classList.add('active');
+    popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function (e) {
+    e.preventDefault();
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
+});
