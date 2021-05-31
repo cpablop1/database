6c4a6128cee8b5412151cf7b8614d356f4e69a2b
Willy Wolfsong <wolfsong.21@outlook.es>
2021-05-31 09:36:45 -0500

    Ocultando el menu con la imagen

---
+++ BD/app/static/js/oculto.js
@@ -0,0 +1,11 @@
+'use strioct'
+
+const privado = document.getElementById('ocultar');
+const hidden = document.getElementById('hidden');
+const foter = document.getElementById('foter');
+privado.style.display = 'none';
+hidden.style.display = 'none';
+
+
+console.log(privado)
+console.log(hidden)
