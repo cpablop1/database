2067cecb239fa18edeb26a8bed7a4f694ee581f4
Willy Wolfsong <wolfsong.21@outlook.es>
2021-05-31 11:59:35 -0500

    Agregando JS

--- BD/app/static/js/acceso.js
+++ BD/app/static/js/acceso.js
@@ -1,44 +1,16 @@
 'use strict'
 
-
-console.log("Genial")
-
-
-const btn_login = document.querySelector('nav.navegacion>a.logeo ');
-
-btn_login.addEventListener('click', () => {
-    console.log("Logeando")
-
-    //Generando el Login
-    const ventana = document.createElement('DIV');
-    const titulo2 = document.createElement('H2');
-    titulo2.textContent = "Iniciar Sesion";
+boton = document.getElementById('botonOcultar')
 
 
-    const texto1 = document.createElement('LABEL');
-    texto1.textContent = 'Nombre';
+function esconder() {
 
-    const inputtext = document.createElement('INPUT');
-    inputtext.placeholder = "Escriba su nombre"
+    const privado = document.getElementById('ocultar');
+    const hidden = document.getElementById('hidden');
+    const foter = document.getElementById('foter');
+    privado.style.display = 'none';
+    hidden.style.display = 'none';
+    console.log(privado)
+    console.log(hidden)
+}
 
-
-    const clave1 = document.createElement('LABEL');
-    clave1.textContent = 'Clave';
-
-    const inputtext2 = document.createElement('INPUT');
-    inputtext2.type = "password"
-
-    ventana.classList.add('login-box');
-    ventana.appendChild(titulo2);
-    ventana.appendChild(texto1);
-    ventana.appendChild(inputtext);
-    ventana.appendChild(clave1);
-    ventana.appendChild(inputtext2);
-
-
-    //Agregarlo al documento
-    const navegacion = document.querySelector('.contenido-hero');
-    navegacion.appendChild(ventana)
-
-
-});
--- BD/app/static/js/oculto.js
+++ BD/app/static/js/oculto.js
@@ -1,10 +1,12 @@
-'use strioct'
+'use strict'
 
 const privado = document.getElementById('ocultar');
 const hidden = document.getElementById('hidden');
 const foter = document.getElementById('foter');
 privado.style.display = 'none';
 hidden.style.display = 'none';
+
+
 
 
 console.log(privado)
