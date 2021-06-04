e996829528b54bd36ef67f1478082a2bffe068b3
Rychy <jorgeajrhh@gmail.com>
2021-06-04 17:30:26 -0600

    Solo idente en crear permiso

--- BD/app/views.py
+++ BD/app/views.py
@@ -904,24 +904,8 @@ def CrearPermisos(request):
                         "mostrar_bitacora_jefe": 0,
                         "jefe": 0}
 
-
-dicccionario = {"nombre": "Cajero",
-                "crud_users": 0,
-                "imprimir_cheque": 1,
-                "anular_cheque": 0,
-                "modificar_cheque": 0,
-                "reporte_cheque": 0,
-                "auditar_user": 0,
-                "admin_cuenta_banc": 0,
-                "auditar_cuenta": 0,
-                "mostrar_bitacora_user": 0,
-                "mostrar_bitacora_group": 0,
-                "mostrar_bitacora_jefe": 0,
-                "jefe": 0}
-
-
-data = rol.Rol_permiso_sup()
-resultado = data.create(dicccionario)
-resultado = data.read(2)
-print(resultado)
-return render(request, 'crear_permiso.html')
+        data = rol.Rol_permiso_sup()
+        resultado = data.create(dicccionario)
+        resultado = data.read(2)
+        print(resultado)
+        return render(request, 'crear_permiso.html')
